05c98bee863fb67f21a929de74a81a2a0ac18a40
Abdias Chafang <abdiaschafang@gmail.com>
2022-10-13 15:27:25 +0200

    refactoring the code

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const stuffRoutes = require("./routes/stuff.router");
 const app = express();
 
-const mongoose = require('mongoose');
-app.use(express.json());
+mongoose
+  .connect(
+    "mongodb+srv://chafy:chafy@cluster0.4jbdi.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -18,35 +25,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Objet créé !",
-  });
-});
+app.use(bodyParser.json());
 
-app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
-});
+app.use("/api/stuff", stuffRoutes);
 
 module.exports = app;
