a962eaa061dbb6b55bd34baed4793a5e2e61aba1
Abdias Chafang <abdiaschafang@gmail.com>
2022-10-13 10:32:46 +0200

    implement final server

--- app.js
+++ app.js
@@ -2,4 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Réponse du serveur" });
+});
+
 module.exports = app;
