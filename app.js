6b230b44b6b3e27bc12cb978c43ba6aa83a1bbbd
Abdias Chafang <abdiaschafang@gmail.com>
2022-10-13 16:59:53 +0200

    update method to perform accepted images

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 const stuffRoutes = require("./routes/stuff.router");
 const userRoutes = require("./routes/user.route");
 const app = express();
@@ -30,5 +31,6 @@ app.use(bodyParser.json());
 
 app.use("/api/stuff", stuffRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- routes/stuff.router.js
+++ routes/stuff.router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const stuffCtrl = require("../controllers/stuff.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
+const multerMiddleware = require("../middlewares/multer.middleware");
 const router = express.Router();
 
 router.get("/", authMiddleware, stuffCtrl.getAllStuff);
-router.post("/", authMiddleware, stuffCtrl.createThing);
+router.post("/", authMiddleware, multerMiddleware, stuffCtrl.createThing);
 router.get("/:id", authMiddleware, stuffCtrl.getOneThing);
-router.put("/:id", authMiddleware, stuffCtrl.modifyThing);
+router.put("/:id", authMiddleware, multerMiddleware, stuffCtrl.modifyThing);
 router.delete("/:id", authMiddleware, stuffCtrl.deleteThing);
 
 module.exports = router;
